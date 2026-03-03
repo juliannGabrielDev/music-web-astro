5366fa5698091d3496df186de8b9f9af92978dc0
jli4n.4ndr0 <jli4n.4ndr0@gmail.com>
2026-03-03 14:28:29 +0000

    Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    plugins: [tailwindcss()],
-  }
+	vite: {
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+		plugins: [tailwindcss()],
+	},
 });
